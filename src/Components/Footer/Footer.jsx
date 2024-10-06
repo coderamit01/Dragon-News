76084d52099702da406d0a2a750a5a2dd5bb9ddd
Amit Hasan <webase.master@gmail.com>
2024-10-06 12:22:35 +0600

    footer design

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const Footer = () => {
-  return (
-    <div>
-     <h2>This is footer</h2>
-    </div>
-  );
+return (
+<div className='bg-base-200 text-base-content py-2'>
+  <div className='container mx-auto px-3'>
+    <p className='text-center font-Poppins text-base'>All Rights Reserved | Amit</p>
+  </div>
+</div>
+);
 };
 
 export default Footer;
